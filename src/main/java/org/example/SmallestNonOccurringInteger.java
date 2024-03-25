ff1aeed3fe54754bce3f9ff7e15d8dd4167867b0
AyeniAmanda <amandaayeni32@gmail.com>
2024-03-25 21:48:12 +0100

    created a method to find the smallest non occurring integer

--- src/main/java/org/example/SmallestNonOccurringInteger.java
+++ src/main/java/org/example/SmallestNonOccurringInteger.java
@@ -1,5 +1,25 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class SmallestNonOccurringInteger {
+
+    public static int findSmallestNonOccurringInteger(Integer[] array) {
+        Arrays.sort(array);
+        int smallestNonOccurring = 1;
+
+        for (int number : array) {
+            if (number <= 0) {
+                continue;
+            }
+            if (number <= smallestNonOccurring) {
+                smallestNonOccurring = number + 1;
+            } else {
+                break;
+            }
+        }
+
+        return smallestNonOccurring;
+    }
 }
 
