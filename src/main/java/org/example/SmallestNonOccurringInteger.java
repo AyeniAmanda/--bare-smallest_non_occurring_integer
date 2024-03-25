6f7b9c4bde92d53e4d1c08ddca91300fec5e0cf1
AyeniAmanda <amandaayeni32@gmail.com>
2024-03-25 22:42:46 +0100

    added the main method to cakk the findSmallestNonOccurringInteger and return the integer

--- src/main/java/org/example/SmallestNonOccurringInteger.java
+++ src/main/java/org/example/SmallestNonOccurringInteger.java
@@ -21,5 +21,20 @@ public class SmallestNonOccurringInteger {
 
         return smallestNonOccurring;
     }
-}
 
+    public static void main(String[] args) {
+
+        Integer[] array1 = {1, 3, 6, 4, 1, 2};
+        Integer[] array2 = {5, -1, -3};
+        Integer[] array3 = {1, 2, 3, 3, 5, 6};
+
+        int smallest1 = findSmallestNonOccurringInteger(array1);
+        int smallest2 = findSmallestNonOccurringInteger(array2);
+        int smallest3 = findSmallestNonOccurringInteger(array3);
+
+        System.out.println("Smallest non-occurring integer in Array1: " + smallest1);
+        System.out.println("Smallest non-occurring integer in Array2: " + smallest2);
+        System.out.println("Smallest non-occurring integer in Array3: " + smallest3);
+    }
+
+}
